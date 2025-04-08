d36621977359a848643624c9a9d0110c5a5aaa53
Xiaoyun-Qiu <qiu.xyun@gmail.com>
2025-04-07 21:03:01 -0500

    Update buy_sell_stock_k.py

--- buy_sell_stock_k.py
+++ buy_sell_stock_k.py
@@ -30,8 +30,24 @@ def max_profit2(prices, k):
                     dp[ind][buy][cap] = max(dp[ind+1][1][cap], prices[ind]+dp[ind+1][0][cap-1])
     return dp[0][0][k]
 
+def max_profit3(prices, k):
+    n = len(prices)
+    # notice that the dimension of dp is (n+1)*2*(k+1) now!! because the continuation value for the last date is zero
+    curr = [[0]*(k+1)  for _ in range(2)]
+    ahead = [[0] * (k + 1) for _ in range(2)]
+    for ind in range(n-1,-1,-1):
+        for buy in range(2):
+            for cap in range(1,k+1):
+                if buy == 0:
+                    ahead[buy][cap] = max(curr[0][cap], -prices[ind]+curr[1][cap])
+                if buy == 1:
+                    ahead[buy][cap] = max(curr[1][cap], prices[ind]+curr[0][cap-1])
+        curr = ahead.copy()
+    return ahead[0][k]
+
 if __name__ == '__main__':
     prices = [3, 3, 5, 0, 0, 3, 1, 4]
     print(max_profit(prices, 2))
     print(max_profit2(prices, 2))
+    print(max_profit3(prices, 2))
 
